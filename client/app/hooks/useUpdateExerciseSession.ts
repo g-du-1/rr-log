c06f30c741212169554f9f52d69875c36ff673b3
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-10-17 23:19:26 +0100

    Fix flicker

--- client/app/hooks/useUpdateExerciseSession.ts
+++ client/app/hooks/useUpdateExerciseSession.ts
@@ -1,20 +1,25 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { fetchWithAuth } from "../util/fetchWithAuth";
+import { ExerciseSession, SavedReps } from "app/types";
 
 export const useUpdateExerciseSession = () => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationFn: async (data: any) => {
+    mutationFn: async (data: { [key: string]: SavedReps }) => {
       return await fetchWithAuth("/exercise-session", {
         method: "PUT",
         body: JSON.stringify({ data }),
       });
     },
     onMutate: (newExerciseSession) => {
-      queryClient.setQueryData(["getExerciseSession"], {
-        data: newExerciseSession,
-      });
+      queryClient.setQueryData(
+        ["getExerciseSession"],
+        (oldData: ExerciseSession) => ({
+          ...oldData,
+          data: newExerciseSession,
+        })
+      );
     },
     onSuccess: (data) => {
       queryClient.setQueryData(["getExerciseSession"], data);
