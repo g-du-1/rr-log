e8344ceea2688097adb9dab24329a0fae8330f6b
Gergo Dudaszeg <dudaszeg1@gmail.com>
2025-10-17 14:24:17 +0100

    Add jpa auditing

--- server/src/main/java/com/gd/exercisetracker/exercisesession/ExerciseSession.java
+++ server/src/main/java/com/gd/exercisetracker/exercisesession/ExerciseSession.java
@@ -4,14 +4,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.EntityListeners;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 public class ExerciseSession {
